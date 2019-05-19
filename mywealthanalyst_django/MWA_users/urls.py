93baa862a89256a19fbd15dfc4c0fd1945a85c46
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-05-19 23:39:04 +1000

    reset password functionality complete

--- mywealthanalyst_django/MWA_users/urls.py
+++ mywealthanalyst_django/MWA_users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,5 +9,6 @@ urlpatterns = [
 	url(r'^login/$', views.login_view, name='login'),
     url(r'^register/$', views.register_view, name='register'),
 	url(r'^logout/$', views.logout_view, name='logout'),
+    path('', include('django.contrib.auth.urls')),  # for password resets
 
         ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
