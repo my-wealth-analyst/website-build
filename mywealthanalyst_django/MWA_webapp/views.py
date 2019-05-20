79d431d24f03ec69219e73d29fd5b5169d72d702
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-05-21 00:29:34 +1000

    historic data and charts for oil, bitcoin, allord_PEratio

--- mywealthanalyst_django/MWA_webapp/views.py
+++ mywealthanalyst_django/MWA_webapp/views.py
@@ -10,6 +10,8 @@ import requests
 
 from .models import Commodities
 
+from scripts.get_historical_data import update_bitcoin
+
 # Create your views here.
 
 def landingpage(request):
@@ -29,6 +31,7 @@ def dashboard(request):
     Bitcoin = Commodities.objects.filter(enabled=True).get(commodity_name='Bitcoin')
     AUD = Commodities.objects.get(commodity_name='AUD')
 
+
     return render(request, 'MWA_webapp/main.html', {'Commodities':All , 'Gold':Gold, 'Silver': Silver , 'Property':Property , 'Oil':Oil , 'AllOrds':AllOrds , 'Bitcoin':Bitcoin, 'AUD':AUD })
 
 @login_required(redirect_field_name='my_redirect_field')
@@ -36,41 +39,42 @@ def get_data(request):
     commodity_one = request.GET.get('commodity_one', None)
     commodity_two = request.GET.get('commodity_two', None)
 
+    print(commodity_two)
+
     filepath_one = os.path.join(BASE_DIR, f"../media_files/datasets/{commodity_one}_askprice_avg_aud.csv")
     filepath_two = os.path.join(BASE_DIR, f"../media_files/datasets/{commodity_two}_askprice_avg_aud.csv")
 
-    try:
-        commodity_one_df = pd.read_csv(filepath_one)
-        commodity_one_df.columns = ['Date',commodity_one]
-        commodity_one_df = commodity_one_df.set_index('Date')
-        commodity_one_df.sort_index(axis=0,ascending=True,inplace=True)
-        commodity_one_df = commodity_one_df.loc[~commodity_one_df.index.duplicated(keep='first')]
+    commodity_one_df = pd.read_csv(filepath_one)
+    commodity_one_df.Date = pd.to_datetime(commodity_one_df.Date)
+    commodity_one_df.columns = ['Date',commodity_one]
+    commodity_one_df = commodity_one_df.set_index('Date')
+    commodity_one_df.sort_index(axis=0,ascending=True,inplace=True)
+    commodity_one_df = commodity_one_df.loc[~commodity_one_df.index.duplicated(keep='first')]
 
+    if commodity_two == 'identity':
+        commodity_two_df = commodity_one_df.copy()
+        commodity_two_df.iloc[:] = 1
+    else:
         commodity_two_df = pd.read_csv(filepath_two)
+        commodity_two_df.Date = pd.to_datetime(commodity_two_df.Date)
         commodity_two_df.columns = ['Date',commodity_one]
         commodity_two_df = commodity_two_df.set_index('Date')
         commodity_two_df.sort_index(axis=0,ascending=True,inplace=True)
         commodity_two_df = commodity_two_df.loc[~commodity_two_df.index.duplicated(keep='first')]
 
+    df = commodity_one_df.merge(commodity_two_df, left_index=True,right_index=True)
+    df.dropna(axis=0,how='any',inplace=True)
 
-        df = commodity_one_df.merge(commodity_two_df, left_index=True,right_index=True)
-        df.dropna(axis=0,how='any',inplace=True)
+    if df[df.columns[0]].sum() > df[df.columns[1]].sum():  # if commodity one nominally 'more valuable' (i.e. higher number) than commodity two
+        df['output'] = df[df.columns[0]] / df[df.columns[1]]
+    else: # if commodity two nominally 'more valuable' (i.e. higher number) than commodity one
+        df['output'] = df[df.columns[1]] / df[df.columns[0]]
 
-        if df[df.columns[0]].sum() > df[df.columns[1]].sum():  # if commodity one nominally 'more valuable' (i.e. higher number) than commodity two
-            df['output'] = df[df.columns[0]] / df[df.columns[1]]
-        else: # if commodity two nominally 'more valuable' (i.e. higher number) than commodity one
-            df['output'] = df[df.columns[1]] / df[df.columns[0]]
+    df.reset_index(inplace=True)
+    df = df[['Date','output']]
+    df.Date = pd.to_datetime(df.Date)
+    df.Date = df.Date.astype(np.int64) // 10**6
 
-        df.reset_index(inplace=True)
-        df = df[['Date','output']]
-        df.Date = pd.to_datetime(df.Date)
-        df.Date = df.Date.astype(np.int64) // 10**6
-
-        df.columns = ['x','y']
-
-        df_jsonformat = [df.values.tolist()]
-
-        return HttpResponse(df_jsonformat)
-
-    except ValueError:
-        return HttpResponse([], content_type = 'application/json')
+    df.columns = ['x','y']
+    df_jsonformat = [df.values.tolist()]
+    return HttpResponse(df_jsonformat)
