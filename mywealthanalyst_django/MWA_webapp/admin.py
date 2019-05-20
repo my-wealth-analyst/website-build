e2f169e7327eb91102b841c8f3934296b63429da
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-05-21 00:29:34 +1000

    historic data and charts for oil, bitcoin, allord_PEratio

--- mywealthanalyst_django/MWA_webapp/admin.py
+++ mywealthanalyst_django/MWA_webapp/admin.py
@@ -2,21 +2,20 @@ from django.contrib import admin
 from .models import (
                     Commodities
                     )
-from scripts.perthmint_getdata import data_update_commodity, delete_commodity_file
+from scripts.get_historical_data import update_gold_and_silver, update_oil, update_bitcoin, update_allords_PE_ratio
 from scripts.scrape_livedata import scrape_all
 
 def refresh_data(ModelAdmin, request, queryset):
-    scrape_all()
+    update_gold_and_silver()
+    update_oil()
+    update_bitcoin()
+    update_allords_PE_ratio()
+
 
 class CommoditiesAdmin(admin.ModelAdmin):
     actions = [refresh_data]
     readonly_fields = ["date_last_scraped"]
     list_display = ['commodity_name','enabled','date_last_scraped','last_price']
-
-
-
-
-
 
 # Register your models here.
 
---
+++ mywealthanalyst_django/scripts/get_historical_data.py
@@ -0,0 +1,138 @@
+import time, datetime
+import requests
+import os, io
+
+from mywealthanalyst_django.settings import BASE_DIR
+
+import pandas as pd
+import numpy as np
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
+
+
+
+def goldsilver_scraperfunction(commodity = None):
+    """
+    Scrapes Perthmint website for historical silver and gold prices.
+    Perth mint stores these as CSV files, in same format for silver and gold.
+    This function is generalised for 'commodity' and is called below for each of 'gold' and 'silver'
+    """
+
+    filepath = os.path.join(BASE_DIR,f"../media_files/datasets/{commodity}_askprice_avg_aud.csv")
+
+    if os.path.isfile(filepath):
+        old_data = pd.read_csv(filepath, parse_dates=True)
+        old_data.Date = pd.to_datetime(old_data.Date)
+        old_data.set_index('Date',inplace=True)
+
+        url = f'http://www.perthmint.com.au/treasury/{commodity}-Current.csv'
+        df = pd.read_csv(url,header=0)
+        df = df[['Market','Perth Mint Spot.15']]
+        df.columns = ['Date','AskPrice_Avg_AUD']
+        df.dropna(axis=0,how='any',inplace=True)
+        df = df.iloc[4:,:]
+        df.Date = pd.to_datetime(df.Date , format = '%d/%m/%y')
+        df.set_index('Date',inplace=True)
+        df = df[~df.index.duplicated()]
+
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.astype(str)
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.str.strip()
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.str.replace(',', '')
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.astype('float')
+
+        new_data = pd.concat((old_data,df),axis=0,join='outer')
+        new_data = new_data[~new_data.index.duplicated()]
+
+        new_data.reset_index(inplace=True)
+        new_data.to_csv(filepath, index=False)
+
+    else:
+        url = f'http://www.perthmint.com.au/treasury/{commodity}.csv'
+        df = pd.read_csv(url,header=0)
+        df = df[['Market','Perth Mint Spot.15']]
+        df.columns = ['Date','AskPrice_Avg_AUD']
+        df.dropna(axis=0,how='any',inplace=True)
+        df = df.iloc[4:,:]
+        df.Date = pd.to_datetime(df.Date , format = '%d/%m/%y')
+        df.set_index('Date',inplace=True)
+
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.astype(str)
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.str.strip()
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.str.replace(',', '')
+        df.AskPrice_Avg_AUD = df.AskPrice_Avg_AUD.astype('float')
+
+        df = df[~df.index.duplicated()]
+        df.reset_index(inplace=True)
+
+        df.to_csv(filepath, index=False)
+
+def update_gold_and_silver():
+    goldsilver_scraperfunction('gold')
+    goldsilver_scraperfunction('silver')
+    goldsilver_scraperfunction('gold')
+    goldsilver_scraperfunction('silver')
+
+def update_oil():
+    """
+    Scrapes Markinsider website for historical WTI crude oil prices (in usd).
+    Data is presented as a single full html table
+    """
+    options = Options()
+    options.add_argument("--disable-notifications")
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.set_page_load_timeout(10)
+
+    filepath = os.path.join(BASE_DIR,f"../media_files/datasets/oil_askprice_avg_aud.csv")
+    date = datetime.date.today().strftime('%d.%m.%Y')
+
+    try:
+        page = driver.get(f'https://markets.businessinsider.com/commodities/historical-prices/oil-price/usd/1.1.2005_{date}?type=wti')
+        time.sleep(5)
+    except TimeoutException:
+        pass
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    table = soup.find('table', {'class':'table instruments'})
+    df = pd.read_html(str(table))[0]
+
+    df = df[['Date','Open']]
+    df.columns = ['Date','AskPrice_Avg_AUD']
+    df.to_csv(filepath, index=False)
+
+def update_bitcoin():
+    """
+    Scrapes Coinmarketcap website for historical bitcoin prices (in usd).
+    Data is presented as a single full html table
+    """
+    filepath = os.path.join(BASE_DIR,f"../media_files/datasets/bitcoin_askprice_avg_aud.csv")
+    date = datetime.date.today().strftime('%Y%m%d')
+
+    url = f'https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130428&end={date}'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    df = pd.read_html(str(soup.find('div',{'class':"table-responsive"}).find('table',{'class':'table'})))[0]
+    df = df[['Date','Open*']]
+    df.columns = ['Date','AskPrice_Avg_AUD']
+    df.to_csv(filepath, index=False)
+
+def update_allords_PE_ratio():
+
+    filepath = os.path.join(BASE_DIR,f"../media_files/datasets/allordsperatio_askprice_avg_aud.csv")
+
+    url = 'https://www.marketindex.com.au/sites/default/files/statistics/asx-fundamentals.xlsx'
+    r = requests.get(url)
+
+    with open(os.path.join(BASE_DIR,f"../media_files/datasets/asx-fundamentals.xlsx"), 'wb') as f:
+        f.write(r.content)
+
+    df = pd.read_excel('asx-fundamentals.xlsx', header=9)
+    df = df[['Date','PE Ratio']]
+    df.columns = ['Date','AskPrice_Avg_AUD']
+    df.dropna(axis=0,how='any',inplace=True)
+
+    df.to_csv(filepath, index=False)
