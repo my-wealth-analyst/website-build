595e156c3dcd454c063b8332991a2a39c734ac74
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-06-02 21:52:01 +1000

    production settings

--- mywealthanalyst_django/mywealthanalyst_django/settings.py
+++ mywealthanalyst_django/mywealthanalyst_django/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'mywealthanalyst_django.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mywealthanalyst',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '104.248.152.99',
+        'PORT': '',
     }
 }
 
