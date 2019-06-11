628b137f62c20adbff6367a9efee8f11ba294a77
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-06-11 23:56:37 +1000

    headless

--- mywealthanalyst_django/MWA_webapp/apps.py
+++ mywealthanalyst_django/MWA_webapp/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from django.conf import settings
 
 class MwaWebappConfig(AppConfig):
     name = 'MWA_webapp'
@@ -14,7 +14,10 @@ class MwaWebappConfig(AppConfig):
         options.add_argument("--disable-notifications")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.headless = True
+        if settings.DEBUG:
+            options.headless = False
+        else:
+            options.headless = True
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         options.add_argument('user-agent={0}'.format(user_agent))
 
