6f971e6f65b0abdd7c9230810f6de66d7599b67f
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-06-12 00:12:58 +1000

    headless display

--- mywealthanalyst_django/MWA_webapp/apps.py
+++ mywealthanalyst_django/MWA_webapp/apps.py
@@ -3,6 +3,8 @@ from django.apps import AppConfig
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from django.conf import settings
+from pyvirtualdisplay import Display
+from selenium import webdriver
 
 class MwaWebappConfig(AppConfig):
     name = 'MWA_webapp'
@@ -10,18 +12,21 @@ class MwaWebappConfig(AppConfig):
 
     def ready(self):
 
+        display = Display(visible=0, size=(800, 600))
+        display.start()
+
         options = Options()
         options.add_argument("--disable-notifications")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        if settings.DEBUG:
-            options.headless = False
-        else:
-            options.headless = True
+        # if settings.DEBUG:
+        #     options.headless = False
+        # else:
+        #     options.headless = True
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         options.add_argument('user-agent={0}'.format(user_agent))
 
         global driver
-        # driver = webdriver.Chrome(chrome_options=options)
-        driver = None
+        driver = webdriver.Chrome(chrome_options=options)
+        # driver = None
         page = driver.get('https://au.investing.com/?ref=www')
