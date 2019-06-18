895de3d62f942439ec176580fa0648087a89eb5f
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-06-18 22:54:48 +1000

    push

--- mywealthanalyst_django/MWA_webapp/tasks.py
+++ mywealthanalyst_django/MWA_webapp/tasks.py
@@ -2,8 +2,10 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 
+from MWA_webapp.models import Commodities
+
 from scripts.scrape_livedata_v2 import scrape_current
-from scripts.get_historical_data_v2 import update_houseprice, update_annualincome
+# from scripts.get_historical_data_v2 import update_houseprice, update_annualincome
 
 logger = get_task_logger(__name__)
 
@@ -35,3 +37,15 @@ def cron_update_houseprice_and_annualincome():
     """
     update_houseprice()
     update_annualincome()
+
+
+@periodic_task(
+    run_every=(crontab(hour=0, minute=20)),
+    name="update_historic_from_live_prices",
+    ignore_result=True
+)
+def update_historic_from_live_prices():
+    """
+    Update ABS house price data for 8 capital cities
+    """
+    Commodities.objects.get(commodity_name='allords')
