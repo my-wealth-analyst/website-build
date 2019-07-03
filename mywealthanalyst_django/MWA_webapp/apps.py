8193192a98cd47cf546e5ed1009863e6a2b4cfe8
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2019-07-03 11:18:15 +1000

    tasks.py debug

--- mywealthanalyst_django/MWA_webapp/apps.py
+++ mywealthanalyst_django/MWA_webapp/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 
-
-
 class MwaWebappConfig(AppConfig):
     name = 'MWA_webapp'
     verbose_name = "MyWealthAnalyst Django Web App"
-
-    def ready(self):
-        pass
