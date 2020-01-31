bef9e4d92aa4348470126b48d7ebe971d87f3360
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2020-01-31 17:39:37 +0000

    recommit settings prod

---
+++ mywealthanalyst_django/mywealthanalyst_django/settings_production.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST_USER = 'dean@mywealthanalyst.com'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = 'Dragon.ba11'
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
